930f44c94a3c5e8d3d125d4f53a9ef31bbaffb32
Volkan Welp <volkanwelp@gmail.com>
2023-05-24 23:15:47 +0200

    ✨ Add all logic

--- monopoly.py
+++ monopoly.py
@@ -2,15 +2,114 @@ import random
 
 
 def play_round(player_spot, data):
-    die_roll = random.randint(1, 6) + random.randint(1, 6)
-    player_spot += die_roll
+    go = True
+    amount_double = 0
+    while go == True:
+        amount_double += 1
 
-    while player_spot + 1 > len(data):
-        player_spot -= len(data)
+        if amount_double == 3:
+            data, player_spot = jail(data, player_spot)
 
-    data[list(data.keys())[player_spot]] += 1
+        amount_roll, go = die_roll()
+
+        player_spot += amount_roll
+
+        while player_spot + 1 > len(data):
+            player_spot -= len(data)
+
+        data[list(data.keys())[player_spot]] += 1
+
+        if list(data.keys())[player_spot] == "Chance":
+            chance = get_chance()
+            if chance == "Jail":
+                data, player_spot = jail(data, player_spot)
+            else:
+                if not isinstance(chance, int):
+                    player_spot = list(data.keys()).index(chance)
+                else:
+                    player_spot += chance
+            if chance != 0:
+                data[list(data.keys())[player_spot]] += 1
+        elif list(data.keys())[player_spot] == "Community Chest":
+            chest = get_chest()
+            if chest == "Jail":
+                data, player_spot = jail(data, player_spot)
+            else:
+                if not isinstance(chest, int):
+                    player_spot = list(data.keys()).index(chest)
+                else:
+                    player_spot += chest
+            if chest != 0:
+                data[list(data.keys())[player_spot]] += 1
+        elif list(data.keys())[player_spot] == "Go To Jail":
+            jail(data, player_spot)
 
     return player_spot, data
+
+
+def die_roll():
+    first = random.randint(1, 6)
+    second = random.randint(1, 6)
+    return (first + second), (first == second)
+
+
+def jail(data, player_spot):
+    data["Jail"] += 3
+    player_spot = list(data.keys()).index("Jail")
+    return data, player_spot
+
+
+def get_chance():
+    chance_cards = [
+        "Go",
+        "Illinois Avenue",
+        "St. Charles Place",
+        "UTILITY",
+        "RAILROAD",
+        0,
+        0,
+        -3,
+        "Jail",
+        0,
+        0,
+        "Reading Railroad",
+        "Boardwalk",
+        0,
+        0,
+        0
+    ]
+
+    choice = random.choice(chance_cards)
+
+    if choice == "UTILITY":
+        return random.choice(["Electric Company", "Water Works"])
+    elif choice == "RAILROAD":
+        return random.choice(["Reading Railroad", "Pennsylvania Railroad", "B. & O. Railroad", "Short Line"])
+    else:
+        return choice
+
+
+def get_chest():
+    chest_cards = [
+        "Go",
+        0,
+        0,
+        0,
+        0,
+        "Jail",
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    ]
+
+    return random.choice(chest_cards)
 
 
 def get_fields():
