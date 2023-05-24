72140aade8d6caa9a63ad3420971e71b5e3881b9
Volkan Welp <volkanwelp@gmail.com>
2023-05-24 22:13:40 +0200

    🔥 Remove unused code

--- barchart.py
+++ barchart.py
@@ -10,13 +10,15 @@ def bar_init(x, y):
 def update_chart(x, data, ax):
     ax.bar(x, data.values(), color='maroon')
 
-    for rect, label in zip(ax.patches, data.values()):
-        height = rect.get_height()
-
     plt.xlabel("Amount landed on square")
     plt.ylabel("Monopoly squares")
     plt.title("Land probability Monopoly")
+
+    plt.xticks(rotation='vertical')
+    plt.tight_layout()
+
     plt.pause(0.1)
     ax.clear()
+
     if not plt.get_fignums():
         quit()
